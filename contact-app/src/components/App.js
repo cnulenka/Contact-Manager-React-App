be3de3405375fcb08a69e252529689a2a082c44d
cnulenka <sinulenka001@gmail.com>
2021-05-27 21:40:57 +0530

    react app setup

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <AddContact />
+    <ContactList />
+    </div>
+  );
+}
+
+export default App;
