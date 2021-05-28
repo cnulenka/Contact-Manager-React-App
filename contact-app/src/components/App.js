38404120e225540c25ec44013bd80b5df36a3040
cnulenka <sinulenka001@gmail.com>
2021-05-28 16:23:26 +0530

    added json server, axios get,post,delete calls

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -6,31 +6,46 @@ import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 import ContactDetail from "./ContactDetails";
+import api from "../api/contacts";
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts"
   const [contacts, setContacts] = useState([]);
 
-  const addContactHandler = (contact) => {
-    setContacts([...contacts, {id:uuid(), ...contact}]);
+  const retriveContacts = async () => {
+    const response = await api.get("/contacts");
+    return response.data;
   }
 
-  const removeContactHandler = (id) => {
+  const addContactHandler = async (contact) => {
+
+    const request = {
+      id: uuid(),
+      ...contact
+    }
+
+    const response = await api.post("/contacts", request);
+    setContacts([...contacts, response.data]);
+  }
+
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`);
     const newContactList = contacts.filter((contact) => {
-      return contact.id !==id;
-    })
+      return contact.id !== id;
+    });
 
     setContacts(newContactList);
   }
 
   useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retriveContacts) setContacts(retriveContacts);
-  }, []);
+    const getAllContacts = async () => {
+      const allContacts = await retriveContacts();
+      if(allContacts) setContacts(allContacts);
+    }
 
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
+    getAllContacts();
+
+  }, []);
 
   return (
     <div className="ui container">
